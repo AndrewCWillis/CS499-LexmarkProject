0963fe70f4502fb8534ac131bf605a1a00cc922a
Spencer Gillaspie <stgi223@uky.edu>
2022-09-26 19:20:19 -0400

    Two buttons on first page

--- team_builder_app/src/App.js
+++ team_builder_app/src/App.js
@@ -1,8 +1,27 @@
+import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack';
+
 const App = () => {
   return (
-    <div className="App">
-      Hello
-    </div>
+    // <div className='App'>
+    //   <Container>
+    //   <Row classname="align-items-center">
+    //     <Col md="auto">
+    //       <Button variant="primary">Input Data</Button>
+    //     </Col>
+    //     <Col md="auto">
+    //       <Button variant="primary">Build a Team</Button>
+    //     </Col>
+    //   </Row>
+    // </Container>
+    // </div>
+    <Stack direction='horizontal' gap={2} className="col-md-5 mx-auto my-auto h-100">
+      <Button className="mx-auto my-auto" variant="primary">Input Data</Button>
+      <Button className="mx-auto my-auto" variant="primary">Build a Team</Button>
+    </Stack>
   );
 }
 
