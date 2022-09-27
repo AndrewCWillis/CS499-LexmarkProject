4c573d3ac510f9641b936bb9ffaaee739135e690
Spencer Gillaspie <stgi223@uky.edu>
2022-09-27 09:35:29 -0400

    Added links to where I found some info

--- team_builder_app/src/App.js
+++ team_builder_app/src/App.js
@@ -6,18 +6,7 @@ import Stack from 'react-bootstrap/Stack';
 
 const App = () => {
   return (
-    // <div className='App'>
-    //   <Container>
-    //   <Row classname="align-items-center">
-    //     <Col md="auto">
-    //       <Button variant="primary">Input Data</Button>
-    //     </Col>
-    //     <Col md="auto">
-    //       <Button variant="primary">Build a Team</Button>
-    //     </Col>
-    //   </Row>
-    // </Container>
-    // </div>
+    // Syling: https://stackoverflow.com/questions/41265182/vertical-alignment-in-bootstrap-4#:~:text=1%20%2D%20Vertical%20Center%20Using%20Auto%20Margins
     <Stack direction='horizontal' gap={2} className="col-md-5 mx-auto my-auto h-100">
       <Button className="mx-auto my-auto" variant="primary">Input Data</Button>
       <Button className="mx-auto my-auto" variant="primary">Build a Team</Button>
