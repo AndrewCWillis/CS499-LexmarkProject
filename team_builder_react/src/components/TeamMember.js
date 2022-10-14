9bd86d9e5a0fff7913fe9bda4b17bcf4abe8cf56
Spencer Gillaspie <stgi223@uky.edu>
2022-10-14 17:29:21 -0400

    Update TeamMembmer to use new CheckList version

--- team_builder_react/src/components/TeamMember.js
+++ team_builder_react/src/components/TeamMember.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CheckList from './CheckList.js'
-class TeamMember extends Component {
-    constructor(props) {
-        super(props);
 
-      }
-    render(){
+const TeamMember = () => {  
+    var techList = [];
+
+    /*
+        Callback function for when the user makes changes to the CheckList.
         
-        return(
-        <>
-            <form>
-                <div className="container">
-                    <div className = "form-label">
-                    Please, Enter the Desired Technical Skills for this Position:
-                    </div>
-                    {<CheckList />}
-                </div>    
-            </form>
-            
-        </>
-        );
+        This function is passed to the CheckList component as a prop and the
+        CheckList will call it when its selected values change.
+    */
+    const GetInputFromCheckList = (skills) => {
+        techList = skills;
     }
+
+    return(
+    <>
+        <form>
+            <div className="container">
+                <div className = "form-label">
+                    Please, Enter the Desired Technical Skills for this Position:
+                </div>
+                {<CheckList SendToParent={GetInputFromCheckList} />}
+            </div>    
+        </form>
+    </>
+    );
 }
 
 export default TeamMember;
