59c7de584e50f53d61206265dd90cb3c68c9b569
SpencerGillaspie <55415019+SpencerGillaspie@users.noreply.github.com>
2022-10-14 17:36:37 -0400

    Add tech to input (#13)
    * Started editing CheckList
    
    * Changed CheckList to function. Added it to input page
    
    * Update TeamMembmer to use new CheckList version
    
    Co-authored-by: Spencer Gillaspie <stgi223@uky.edu>

--- team_builder_react/src/components/CheckList.js
+++ team_builder_react/src/components/CheckList.js
@@ -1,31 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Multiselect from 'multiselect-react-dropdown';
 
-class CheckList extends Component {
-    constructor(props) {
-        super(props);
-        const skills = ['JavaScript', 'React', 'Python', 'HTML', 'Bootstrap', 'JQuery', 'DJango']
-        this.state = {
-          options: skills.map((skill, index) => ({'name' : skill, 'id' : index}))
-        }
-      }
-    onSelect(selectedList, selectedItem) {
-       console.log("Here")
-    }
+/*
+  This component renders a multiselect-react-dropdown component.
+
+  props: SendToParent - a callback function that this component will call whenever
+          the selected values in the multiselect-react-dropdown changes. The
+          function should take a single parameter, a list.
+
+  https://www.geeksforgeeks.org/how-to-pass-data-from-child-component-to-its-parent-in-reactjs/
+*/
+const CheckList = ( { SendToParent }) => {
+  const skills = ['JavaScript', 'React', 'Python', 'HTML', 'Bootstrap', 'JQuery', 'DJango'];
+  var options = skills.map((skill, index) => ({'name' : skill, 'id' : index}));
+
+  /*
+    Callback function for when the user selects a new item.
+  */
+  const onSelect = (selectedList, selectedItem) => {
+    // console.log(selectedList);
+    // console.log(selectedItem);
+    SendToParent(selectedList);
+  }
+  
+  /*
+    Callback function for when the user deselects an item.
+  */
+  const onRemove = (selectedList, removedItem) => {
+    // console.log("Here")
+    SendToParent(selectedList)
+  }
+
     
-    onRemove(selectedList, removedItem) {
-      console.log("Here")
-    }
-    render(){
-        return(
-          <Multiselect
-            options={this.state.options} // Options to display in the dropdown
-            selectedValues={this.state.selectedValue} // Preselected value to persist in dropdown
-            onSelect={this.onSelect} // Function will trigger on select event
-            onRemove={this.onRemove} // Function will trigger on remove event
-          displayValue="name" // Property name to display in the dropdown options
-          />
-        );
-    }
+  return(
+    <Multiselect
+      options={options} // Options to display in the dropdown
+      onSelect={onSelect} // Function will trigger on select event
+      onRemove={onRemove} // Function will trigger on remove event
+      displayValue="name" // Property name to display in the dropdown options
+      avoidHighlightFirstOption={true}
+    />
+  );
 }
+
 export default CheckList;
