bfe6b3d6e06bc9b7e5f52ee2cd13541a2d9ec77f
Andrew C. Willis <75267685+AndrewCWillis@users.noreply.github.com>
2022-10-18 13:18:00 -0400

    added beginning code to replace a team member. (#14)

--- team_builder_react/src/components/TeamList.js
+++ team_builder_react/src/components/TeamList.js
@@ -9,7 +9,18 @@ function TeamList(props) {
         event.target.parentElement.remove()
     }
     function addHandler(event){
-        console.log("Tried to replace a team member.")
+        var name = event.target.parentElement.id
+        //robert is hard coded now, but will be whatever member was last clicked
+        var replacedName = document.getElementById('Robert').querySelector('.memberName').textContent
+        console.log(`Tried replace a team member with ${name}.`)
+        //will become more complicated later, but just starting off: 
+        document.getElementById('Robert').querySelector('.memberName').textContent = name
+        document.getElementById('Robert').querySelector('.memberName').id = name
+
+        //then, the replaced member moves to the supplemental list:
+        console.log(`${replacedName}`)
+        event.target.parentElement.id = replacedName
+        event.target.parentElement.querySelector('.memberName').textContent = replacedName
     }
 
     var TeamList = ['Robert', 'Spencer', 'Wade', 'Alex', 'Andrew']
@@ -31,8 +42,8 @@ function TeamList(props) {
             TeamList.map(
                 function (name) {
                     return(
-                    <ButtonGroup aria-label="Basic example" key={name}>
-                        <Button variant={props.type} size = "lg" >{name}</Button>
+                    <ButtonGroup aria-label="Basic example" key={name} id = {name}>
+                        <Button variant={props.type} size = "lg" className = 'memberName'>{name}</Button>
                         <Button variant={props.type} size = "sm" onClick = {props.type == "info" ? removeHandler : addHandler} style = {props.type == "info" ? remove : assign}>{props.type == "info" ? 'X' : '+'}</Button>
                     </ButtonGroup>
                     )
