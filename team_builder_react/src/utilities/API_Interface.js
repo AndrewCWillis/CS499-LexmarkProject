94157f59a1e59017b15400c2c63eb5f1ea84bc60
Spencer Gillaspie <stgi223@uky.edu>
2022-10-23 17:11:10 -0400

    Moved skill object to string algo into helper func. Added func for sending team params

--- team_builder_react/src/utilities/API_Interface.js
+++ team_builder_react/src/utilities/API_Interface.js
@@ -5,6 +5,31 @@ const axiosInstance = axios.create({
 });
 
 /*
+    CheckListArrayToString takes an array of objects as a parameter and returns
+        a comma separated string where each value in the string is the name of a skill
+        in the passed in array.
+     
+    parameters: listOfSkills - an array of objects. Each element is { name: string, id: int }
+                    (as given by the CheckList component) and represents a
+                    technical skill that the build team should posses, if possible.
+    
+    returns: a comma separated string of skills
+*/
+const CheckListArrayToString = (listOfSkills) => {
+    var skillsString = "";
+
+    listOfSkills.forEach(skill => {
+        if (skillsString.length === 0){
+            skillsString = skill.name;
+        } else {
+            skillsString = skillsString + "," + skill.name;
+        }
+    });
+
+    return skillsString;
+}
+
+/*
     SendPersonToBackEnd takes an object as a parameter and sends that object to 
         the back-end as JSON in an HTTP request. This function will post a new
         person and their information to the back-end, which will then store it in
@@ -17,24 +42,26 @@ const axiosInstance = axios.create({
             skills: [], each element is { name: string, id: int }
             traits: [], each element is { name: string, score: int}
         }
+    
+    returns: nothing
 
     What will actually be sent to the back-end:
     {
-            "id": 49902,
-            "name_last": "Bar",
-            "name_first": "Foo",
-            "skills": "Python, Django",
-            "bpt_confidence": 0.1,
-            "bpt_delegator": 0.2,
-            "bpt_determination": 0.3,
-            "bpt_disruptor": 0.4,
-            "bpt_independence": 0.5,
-            "bpt_knowledge": 0.6,
-            "bpt_profitability": 0.7,
-            "bpt_relationship": 0.8,
-            "bpt_risk": 0.9,
-            "bpt_selling": 1.0
-        }
+        "id": 49902, Do we need?
+        "name_last": "Bar",
+        "name_first": "Foo",
+        "skills": "Python, Django",
+        "bpt_confidence": 0.1,
+        "bpt_delegator": 0.2,
+        "bpt_determination": 0.3,
+        "bpt_disruptor": 0.4,
+        "bpt_independence": 0.5,
+        "bpt_knowledge": 0.6,
+        "bpt_profitability": 0.7,
+        "bpt_relationship": 0.8,
+        "bpt_risk": 0.9,
+        "bpt_selling": 1.0
+    }
 */
 export const SendPersonToBackEnd = (person) => {
     var objectToSend = {
@@ -44,13 +71,7 @@ export const SendPersonToBackEnd = (person) => {
     };
 
     // Add the skill name for each skill the user selected
-    person.skills.forEach(skill => {
-        if (objectToSend.skills.length === 0){
-            objectToSend.skills = skill.name;
-        } else {
-            objectToSend.skills = objectToSend.skills + "," + skill.name;
-        }
-    });
+    objectToSend.skills = CheckListArrayToString(person.skills);
 
     person.traits.forEach(trait => {
         objectToSend["bpt_" + trait.name] = trait.score;
@@ -58,4 +79,36 @@ export const SendPersonToBackEnd = (person) => {
 
     console.log(objectToSend);
     //axiosInstance.post('/employees', objectToSend);
+}
+
+/*
+    SendTeamParameters takes the parameters by which a new team will be built as
+        parameters to the function. This function will put the parameters into a
+        JSON object and POST it to the back-end so it can start the process of
+        building a balanced team with those parameters
+     
+    parameters: teamSize - integer representing the number of team members the
+                    built team should have.
+
+                techSkills - an array of objects. Each element is { name: string, id: int }
+                    (as given by the CheckList component) and represents a
+                    technical skill that the build team should posses, if possible.
+
+    returns: nothing
+
+    What will actually be sent to the back-end:
+    {
+        "id": int, Do we need?
+        "teamSize": int
+        "skills": comma separated string of skills
+    }
+*/
+export const SendTeamParameters = (teamSizeParam, techSkills) => {
+    var objectToSend = {
+        teamSize: teamSizeParam,
+        skills: CheckListArrayToString(techSkills)
+    };
+
+    console.log(objectToSend);
+    //axiosInstance.post('/requestedteams', objectToSend);
 }
