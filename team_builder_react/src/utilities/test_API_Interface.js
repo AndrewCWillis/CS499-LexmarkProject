bf6bf833d21e86328c61dce0cdc3db86ad8341f0
SpencerGillaspie <55415019+SpencerGillaspie@users.noreply.github.com>
2022-11-11 12:45:28 -0500

    Spencer indiv feature test (#30)
    * Added a file to test the API_Interface functions
    
    * Removed module type and added comment. Removed expand from header
    
    Co-authored-by: Spencer Gillaspie <stgi223@uky.edu>

---
+++ team_builder_react/src/utilities/test_API_Interface.js
@@ -0,0 +1,22 @@
+/*
+    A file that can be used to call some of the API_Interface functions and view
+    what they return.
+
+    Note: So that you can run this file, you must add `"type": "module",` to the
+    package.json file in the top-level directory of the react application. The
+    React application will not run, though, with this included, so be sure to
+    remove that line when you are done using this file and wish to run the front-end.
+*/
+
+import { SendTeamParameters,
+        GetValidTeam,
+        GetEmployeeList } from "./API_Interface.js";
+
+SendTeamParameters(3, [{name: 'React', id: 1}, {name: 'Python', id: 2}])
+.then((teamId) => {
+    console.log("Response for Sending Team Parameters: " + teamId);
+    GetValidTeam(teamId).then((team) => {
+        console.log("Response for Getting a Built Team: " + JSON.stringify(team.data.team));
+        GetEmployeeList(team.data.team).then(response => console.log("Response for Getting the Names of the People on the Team: " + JSON.stringify(response, null, 4)));
+    });
+});
